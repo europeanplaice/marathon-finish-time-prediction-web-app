2920527641bb068563c24901c780835fd2c38a18
europeanplaice <paul.thomas.goodfellas@gmail.com>
2021-08-17 23:54:47 +0900

    Update hello

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return "<p>hello world<p>"
+def hello_world(name="Tomo"):
+    return render_template("hello.html", name=name)
