e56815e91d9506235829dd8b366701527b576a59
europeanplaice <paul.thomas.goodfellas@gmail.com>
2021-08-18 23:32:27 +0900

    improve

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
+import argparse
+
+from finish_time_predictor import Decoder, Encoder, FinishTimePredictor
+from utils import (makedataset, preprocess_rawdata)
+from main import main
 
 app = Flask(__name__)
 
@@ -7,3 +12,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world(name="Tomo"):
     return render_template("hello.html", name=name)
+
+
+@app.route('/submit', methods=['POST'])
+def process():
+    request = Request
+    main(["--do_predict", "--elapsed_time", data])
+
+
---
+++ main.py
@@ -0,0 +1,71 @@
+import argparse
+
+import pandas as pd
+import seaborn as sns
+
+from finish_time_predictor import Decoder, Encoder, FinishTimePredictor
+from utils import (makedataset, preprocess_rawdata)
+
+sns.set(font="ricty diminished")
+
+
+def main(param=None):
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--do_train", action="store_true")
+    parser.add_argument("--do_eval", action="store_true")
+    parser.add_argument("--do_predict", action="store_true")
+    parser.add_argument("--do_all_splits_eval", action="store_true")
+    parser.add_argument("--train_data_path", default='boston2017-2018.csv')
+    parser.add_argument("--elapsed_time")
+    parser.add_argument("--elapsed_time_what_if")
+    parser.add_argument("--batch_size", default=256, type=int)
+    parser.add_argument("--hidden_size", default=128, type=int)
+    parser.add_argument("--full_record")
+    parser.add_argument("--graph_save_path", default="estimation.jpg")
+    parser.add_argument("--encoder_model_path",
+                        default='trained_model/encoder/encoder')
+    parser.add_argument("--decoder_model_path",
+                        default='trained_model/decoder/decoder')
+
+    if param is not None:
+        args = parser.parse_args(param)
+    else:
+        args = parser.parse_args()
+    if args.elapsed_time is not None:
+        args.do_predict = True
+    encoder = Encoder(args)
+    decoder = Decoder(args)
+    finish_time_predictor = FinishTimePredictor(encoder, decoder)
+    if args.do_train or args.do_eval:
+
+        df = pd.read_csv(args.train_data_path)
+        # save_feather(df)
+        data = preprocess_rawdata(df)
+
+        train_dataset, eval_dataset, eval_onebatch_dataset = \
+            makedataset(data, args, True)
+        if args.do_train:
+            finish_time_predictor.train(
+                train_dataset, eval_dataset, args)
+    if args.do_eval:
+        finish_time_predictor.load_weights(args)
+        finish_time_predictor.validate(eval_onebatch_dataset, args)
+    if args.do_predict:
+        finish_time_predictor.load_weights(args)
+        if args.elapsed_time_what_if is not None:
+            finish_time_predictor.predict(
+                args.elapsed_time.split(","),
+                args,
+                args.elapsed_time_what_if.split(","),
+            )
+        else:
+            finish_time_predictor.predict(
+                args.elapsed_time.split(","),
+                args,
+            )
+
+
+if __name__ == '__main__':
+    main()
